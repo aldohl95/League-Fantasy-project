0526280dbf8e73ca4c2467146ab7dc1a03348a01
aldohl95 <mathias.tevita@icloud.com>
2025-03-25 15:53:01 -0700

    Add files via upload

---
+++ league_zone/LeagueZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.league.league_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
